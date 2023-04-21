e2c6faf05dcfc2ffd1dbd5e7fd4255ff3154d9ac
Mark <mbognar@maple.ca>
2023-04-20 20:12:21 -0400

    Add ProblemPDFs test and updated pipeline

---
+++ Maple.iText.ProblemPDF.tests/ProblemPDFTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace iText.ProblemPDF.tests
+{
+    [TestClass]
+    public class ProblemPDFTests
+    {
+        [TestMethod]
+        public void TestProblemPDFs()
+        {
+            // Arrange
+            string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            string strWorkPath = GetParent(strExeFilePath, "ProblemPDFs");
+            Console.WriteLine($"Path: {strWorkPath}");
+            DirectoryInfo d = new DirectoryInfo($"{strWorkPath}");
+
+            FileInfo[] Files = d.GetFiles("*.pdf");
+
+            var hitFile = false;
+
+            foreach (FileInfo file in Files)
+            {
+                hitFile = true;
+
+                // Act
+                var fileStream = file.OpenRead();
+                iText.Kernel.Pdf.PdfDocument Doc = new iText.Kernel.Pdf.PdfDocument(new iText.Kernel.Pdf.PdfReader(fileStream).SetUnethicalReading(true));
+                var numPages = Doc.GetNumberOfPages();
+
+                // Assert
+                Assert.IsTrue(numPages > 0);
+            }
+
+            // Sanity check to make sure PDF files are processed
+            Assert.IsTrue(hitFile);
+        }
+
+        private string GetParent(string path, string parentName)
+        {
+            var dir = new DirectoryInfo(path);
+
+            if (dir.Parent == null)
+            {
+                return null;
+            }
+
+            if (dir.Parent.Name == parentName)
+            {
+                return dir.Parent.FullName;
+            }
+
+            return GetParent(dir.Parent.FullName, parentName);
+        }
+    }
+}
---
+++ Maple.iText.ProblemPDF.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Maple.iText.ProblemPDF.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Maple Reinders")]
+[assembly: AssemblyProduct("Maple.iText.ProblemPDF.tests")]
+[assembly: AssemblyCopyright("Copyright © Maple Reinders 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f09428fe-fc2b-48c7-a8e3-1c7db2300ee3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
